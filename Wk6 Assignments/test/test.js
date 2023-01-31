794952257c34bd806a3d1ddbc6c64dc2e3c8193c
Jennifer Hanson <jenniferhanson.sd@gmail.com>
2023-01-30 19:00:50 -0800

    updated game file

---
+++ Wk6 Assignments/test/test.js
@@ -0,0 +1,58 @@
+//assert library
+var expect = chai.expect;
+var player1 = [1, 2, 3, 4, 5]
+var player2 = [1, 2, 3, 4, 5]
+import {createDeck} from '.Week-06-War-Card-Game.js'
+
+
+//does the deck shuffle and put out random cards?
+describe('shuffle', function(){
+    describe{'shuffle', function(){
+        it("start should shuffle the deck and put out random card for each player", function(){
+            const x = Player;
+            expect(x).to.have.all.members(player1);
+        })
+    }}
+})
+
+
+// does the card have 3 named arguments
+    // expected: yes, 3 properties of suit, face and rank
+/*
+// does the createDeck function have 52 cards
+    // expected: yes
+describe('Deck test', function(){
+    let deck = new Deck;
+    it("There should be 52 cards in a deck", function(){
+        assert.isArray(deck.createDeck(), == [])
+    })
+})
+// does each card contain all 3 of the card properties
+    // expected: yes, each card should be a unique combintion of suit, face and rank
+describe('Deck test 1', function(){
+    let deck = new Deck;
+    it("There should be a suit, face and rank for each card", function(){
+        assert.isArray(deck.this.cards(suits[], faces[], rank[]))
+    }),,
+})
+// does the shuffleDeck give out a random card
+    // expected: yes, each turn should turn out a unique card for each player have a name
+describe('Deck test 3', function(){
+    let deck = new Deck;
+    it("After shuffle the deck should generate a random card", function(){
+        assert.is
+    })
+})
+// does each player have a 26 cards
+    // expected: yes
+
+// is there a score for each player
+    // expected: yes
+
+// can a player have a score less than zero
+    // expected: no 
+
+// is there a mechanism to declare a winner
+    // expected: yes
+*/
+
