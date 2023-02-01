6dae87d40eed6482f8622e07e01cd4bf103c799f
Jennifer Hanson <jenniferhanson.sd@gmail.com>
2023-01-31 16:47:07 -0800

    final commit

--- Wk6 Assignments/test/test.js
+++ Wk6 Assignments/test/test.js
@@ -1,58 +1,52 @@
-//assert library
-var expect = chai.expect;
-var player1 = [1, 2, 3, 4, 5]
-var player2 = [1, 2, 3, 4, 5]
-import {createDeck} from '.Week-06-War-Card-Game.js'
+var assert = chai.assert;
 
-
-//does the deck shuffle and put out random cards?
-describe('shuffle', function(){
-    describe{'shuffle', function(){
-        it("start should shuffle the deck and put out random card for each player", function(){
-            const x = Player;
-            expect(x).to.have.all.members(player1);
-        })
-    }}
-})
-
-
-// does the card have 3 named arguments
-    // expected: yes, 3 properties of suit, face and rank
-/*
 // does the createDeck function have 52 cards
     // expected: yes
+
 describe('Deck test', function(){
     let deck = new Deck;
+    deck.createDeck();
     it("There should be 52 cards in a deck", function(){
-        assert.isArray(deck.createDeck(), == [])
+        console.log(deck.cards.length);
+        assert(deck.cards.length == 52)
     })
-})
+ })
+
 // does each card contain all 3 of the card properties
     // expected: yes, each card should be a unique combintion of suit, face and rank
-describe('Deck test 1', function(){
-    let deck = new Deck;
+describe('Card test', function(){
+    let card = new Card('♥', "Ace", 1);
     it("There should be a suit, face and rank for each card", function(){
-        assert.isArray(deck.this.cards(suits[], faces[], rank[]))
-    }),,
-})
-// does the shuffleDeck give out a random card
-    // expected: yes, each turn should turn out a unique card for each player have a name
-describe('Deck test 3', function(){
-    let deck = new Deck;
-    it("After shuffle the deck should generate a random card", function(){
-        assert.is
+        assert(card.suit && card.face && card.rank)
     })
 })
+
 // does each player have a 26 cards
     // expected: yes
+describe('number of cards test', function(){
+    let board = new Board();
+    board.start("Player1", "Player2");
+    it('Each player should have 26 cards', function(){
+        assert(board.players[0].playerCards.length == 26 && board.players[1].playerCards.length == 26)
+    })
+})
+
 
 // is there a score for each player
     // expected: yes
+describe('number of cards test', function(){
+    let board = new Board();
+    board.endGame();
+    it('Each player should have a score', function(){
+        assert(score.Player1 !== 0 && score.Player2 !== 0)
+    })
+})
 
+    
 // can a player have a score less than zero
     // expected: no 
 
 // is there a mechanism to declare a winner
     // expected: yes
-*/
+
 
